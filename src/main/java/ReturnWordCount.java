5c8d5afe31e019b1abe47efa0ad6ff92cf7052f1
Vincent Bui <vincent.bui657@gmail.com>
2023-08-03 01:07:01 +0000

    Completed "Word Count"

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,10 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        //Split the input string by spaces to get an array of words
+        String[] words = in.split("\\s+");
+
+        //Return number of words in array
+        return words.length;
     }
 }
